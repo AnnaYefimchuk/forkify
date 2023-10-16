9fcf49767976c1c15277bd45920d7bf4039408b4
annayefimchuk <hanna.yefimchuk@gmail.com>
2023-10-16 16:55:38 +0300

    Init commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '1f063aa7-0bf5-4d32-82ef-cfc22cf42ec6';
+export const MODAL_CLOSE_SEC = 2.5;
